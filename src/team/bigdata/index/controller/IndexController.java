aa8117b8c9914b95cd84b65b97c43355c2c0c688
t1t1 <cha@hanmail2.net>
2017-02-04 05:27:04 +0900

    add root page

--- src/team/bigdata/index/controller/IndexController.java
+++ src/team/bigdata/index/controller/IndexController.java
@@ -1,4 +1,4 @@
-package team.bigdata.sns.controller;
+package team.bigdata.index.controller;
 
 import java.io.IOException;
 
@@ -19,15 +19,15 @@ import team.bigdata.sns.service.SnsUpdate;
 /**
  * Servlet implementation class SnsFrontController
  */
-@WebServlet({"*.do"})
-public class SnsController extends HttpServlet {
+//@WebServlet({"/"}) // FIXME: static files도 처리 됨 
+public class IndexController extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public SnsController() {
+	public IndexController() {
 		super();
 	}
 
@@ -46,46 +46,8 @@ public class SnsController extends HttpServlet {
 		String path = null;
 
 		// url 별 MVC 처리
-		if (url.equals("/getAllinfo.do")) {
-			service = new SnsList();
-			try {
-				service.execute(request, response);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			path = "getAllinfo.jsp";
-		} else if (url.equals("/keulWriting.do")) {
-			service = new SnsCreate();
-			try {
-				service.execute(request, response);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			path = "keulWriting.jsp";
-		} else if (url.equals("/keulSearchOne.do")) {
-			service = new SnsRead();
-			try {
-				service.execute(request, response);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			path = "keulSearchOne.jsp";
-		} else if (url.equals("/keulUpdating.do")) {
-			service = new SnsUpdate();
-			try {
-				service.execute(request, response);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			path = "getAllinfo.do"; // List로 리다이렉트
-		} else if (url.equals("/keulDeleting.do")) {
-			service = new SnsDelete();
-			try {
-				service.execute(request, response);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			path = "getAllinfo.do"; // List로 리다이렉트
+		if (url.equals("/")) {
+			path = "userLogin.lhj";
 		}
 		
 		// response 응답처리
